b77c9cd595e3636eae05e58ec0810cace9a58c06
rkimmi <kimmirindel97@gmail.com>
2018-03-29 16:02:27 +1300

    sound playing dependant on hover

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import Sound from 'react-sound'
 
 class App extends React.Component {
-   constructor (props) {
-       super (props)
-   }
+    constructor(props) {
+        super(props)
+        this.state = {
+            isHovered: false,
+            soundPlaying: 'sounds/vanessa.wav',
+            playStatus: Sound.status.PAUSED
+
+        }
+        this.makeSound = this.makeSound.bind(this)
+        this.pauseSound = this.pauseSound.bind(this)
+    }
+ 
+    makeSound() {
+        this.setState({
+            isHovered: true,
+            playStatus: Sound.status.PLAYING
+        })
+    }
+
+    pauseSound() {
+        this.setState({
+            isHovered: false,
+            playStatus: Sound.status.PAUSED
+        })
+    }
 
     render() {
         return (
@@ -11,7 +34,8 @@ class App extends React.Component {
                 <h4>Seasonal Affective Thinking</h4>
                 <div className='light-container'>
                     <div className='van-light'>V</div>
-                    <div className='kimmi-light'>K</div>
+                    <Sound url={this.state.soundPlaying} playStatus={this.state.playStatus} /> 
+                    <div className='kimmi-light' onMouseEnter={this.makeSound} onMouseLeave={this.pauseSound}>K</div>
                 </div>
             </div>
         )
