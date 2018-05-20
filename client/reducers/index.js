b4b4d2c02a8539a4ef010a48311b79b5a2da9931
Kimmi <kimmi@hmsnz.com>
2018-05-21 11:49:01 +1200

    removed non-existant imports

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,9 +1,7 @@
 import {combineReducers} from 'redux'
 
-import users from './users'
 import errorMessage from './error-message'
 
 export default combineReducers({
-    users,
     errorMessage
 })
